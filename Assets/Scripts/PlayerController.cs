8f62e58c92a522924fe69df03ec9de1946e4972b
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-09-24 11:41:40 +0300

    🔧 (PlayerController.cs): refactor code to improve readability and maintainability by separating logic for moving and rotating the player 💡 (PlayerController.cs): add comments to clarify the purpose of the movePlayer, rotatePlayer, and rotateGunArm functions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,10 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     private Vector2 moveInput;
+    Vector3 mousePosition;
+    Vector3 screenPoint;
 
     public Rigidbody2D rb;
-
     public Transform gunArm;
 
     // Start is called before the first frame update
@@ -21,11 +22,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        mousePosition = Input.mousePosition;
+        screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+
         movePlayer();
+        rotatePlayer();
         rotateGunArm();
     }
 
-    void movePlayer() {
+    void movePlayer()
+    {
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
         moveInput.Normalize();
@@ -33,10 +39,22 @@ public class PlayerController : MonoBehaviour
         rb.velocity = moveInput * moveSpeed;
     }
 
-    void rotateGunArm() {
-        Vector3 mousePosition = Input.mousePosition;
-        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+    void rotatePlayer()
+    {
+        if (mousePosition.x < screenPoint.x && transform.localScale.x == 1f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            gunArm.localScale = new Vector3(-1f, -1f, 1f);
+        }
+        else if (mousePosition.x >= screenPoint.x && transform.localScale.x == -1f)
+        {
+            transform.localScale = Vector3.one;
+            gunArm.localScale = Vector3.one;
+        }
+    }
 
+    void rotateGunArm()
+    {
         Vector2 offset = mousePosition - screenPoint;
         float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
         gunArm.rotation = Quaternion.Euler(0, 0, angle);
