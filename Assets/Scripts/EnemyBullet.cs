67b5db87c08558eb1552c539bb4023cc90ca7a0f
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-09-25 21:56:57 +0300

    ✨ (EnemyBullet.cs): Add EnemyBullet class to handle enemy bullets behavior in the game 📝 (EnemyBullet.cs.meta): Add metadata for EnemyBullet.cs to specify file format version and other necessary information

---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float speed;
+    private Vector3 direction;
+
+    void Start()
+    {
+        direction = PlayerController.instance.transform.position - transform.position;
+        direction.Normalize();
+    }
+
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            // Damage player
+        }
+
+        Destroy(gameObject);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
