315c93bf7f51bc8aac21d629cbbd92bec54c4711
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-09-26 10:23:02 +0300

    ✨ (UIController.cs): create a new UIController script to manage UI elements in the game 📝 (UIController.cs.meta): add metadata for the UIController script to define file format version and other necessary information

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance;
+
+    public Slider healthSlider;
+    public Text healthText;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
