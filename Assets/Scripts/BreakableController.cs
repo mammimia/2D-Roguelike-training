9d589b4c79659aa19708c8f937cee2b9cadb8cd0
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-09-27 13:59:53 +0300

    ✨ (SampleScene.unity): Add functionality to drop items from breakable objects with a configurable drop rate and items to drop 🔧 (BreakableController.cs): Refactor item drop logic to use a boolean flag for determining if items should drop and add an array of dropable items with a configurable drop rate 🔧 (HealthPickup.cs): Add a delay before the health pickup can be collected to prevent immediate collection upon contact

--- Assets/Scripts/BreakableController.cs
+++ Assets/Scripts/BreakableController.cs
@@ -8,27 +8,41 @@ public class BreakableController : MonoBehaviour
     public GameObject[] brokenPiece;
     public int maxPieces = 5;
 
-    void Start()
-    {
-
-    }
-
-    void Update()
-    {
-
-    }
+    public bool shouldDrop;
+    public GameObject[] dropableItems;
+    public float dropRate;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player" && PlayerController.instance.isDashing())
         {
             Destroy(gameObject);
-            int piecesToDrop = Random.Range(1, maxPieces);
-            for (int i = 0; i < piecesToDrop; i++)
+            handleBrokenPieces();
+            handleItemDrop();
+        }
+    }
+
+    private void handleBrokenPieces()
+    {
+        int piecesToDrop = Random.Range(1, maxPieces);
+        for (int i = 0; i < piecesToDrop; i++)
+        {
+            int selectedPiece = Random.Range(0, brokenPiece.Length);
+            Instantiate(brokenPiece[selectedPiece], transform.position, transform.rotation);
+        }
+    }
+
+    private void handleItemDrop()
+    {
+        if (shouldDrop)
+        {
+            float dropChance = Random.Range(0f, 100f);
+            if (dropChance < dropRate)
             {
-                int selectedPiece = Random.Range(0, brokenPiece.Length);
-                Instantiate(brokenPiece[selectedPiece], transform.position, transform.rotation);
+                int itemToDrop = Random.Range(0, dropableItems.Length);
+                Instantiate(dropableItems[itemToDrop], transform.position, transform.rotation);
             }
         }
     }
+
 }
--- Assets/Scripts/HealthPickup.cs
+++ Assets/Scripts/HealthPickup.cs
@@ -6,9 +6,19 @@ public class HealthPickup : MonoBehaviour
 {
     public int healAmount;
 
+    public float delayToBeCollected = 0.5f;
+
+    private void Update()
+    {
+        if (delayToBeCollected > 0)
+        {
+            delayToBeCollected -= Time.deltaTime;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && delayToBeCollected <= 0)
         {
             PlayerHealthController.instance.heal(healAmount);
             Destroy(gameObject);
