dfb634c832458686880742f38b815d368c95bfd9
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-09-29 07:48:03 +0300

    ✨ (CameraController.cs): Add CameraController script to handle camera movement towards a target in the game 📝 (CameraController.cs.meta): Add meta file for CameraController script to define file format version and GUID for asset management

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public static CameraController instance;
+
+    public float moveSpeed;
+
+    public Transform target;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), moveSpeed * Time.deltaTime);
+        }
+    }
+}
