4962ca399cb7960036eeb756f80302c3b1401c39
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-09-29 08:22:53 +0300

    🔧 (RoomController.cs): Add new boolean variables and lists for room control logic 🔧 (RoomController.cs): Implement functionality to close doors when player enters the room and open them when enemies are cleared

--- Assets/Scripts/RoomController.cs
+++ Assets/Scripts/RoomController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class RoomController : MonoBehaviour
 {
+    public bool closeWhenEntered, openWhenEnemiesCleared;
+
+    public GameObject[] doors;
+
+    public List<GameObject> enemies = new List<GameObject>();
+
+    private bool roomActive;
+
     void Start()
     {
 
@@ -11,7 +19,27 @@ public class RoomController : MonoBehaviour
 
     void Update()
     {
+        if (roomActive && enemies.Count > 0 && openWhenEnemiesCleared)
+        {
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                if (enemies[i] == null)
+                {
+                    enemies.RemoveAt(i);
+                    i--;
+                }
+            }
 
+            if (enemies.Count == 0)
+            {
+                foreach (GameObject door in doors)
+                {
+                    door.SetActive(false);
+                }
+
+                closeWhenEntered = false;
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -19,6 +47,24 @@ public class RoomController : MonoBehaviour
         if (other.tag == "Player")
         {
             CameraController.instance.setTarget(transform);
+
+            if (closeWhenEntered)
+            {
+                foreach (GameObject door in doors)
+                {
+                    door.SetActive(true);
+                }
+            }
+
+            roomActive = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            roomActive = false;
         }
     }
 }
