9b5a7f938b6ddabda95437b9cfb3755e74e07387
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-10-01 08:41:20 +0300

    📝 (Title Menu.unity): Add Title Menu scene with occlusion culling and render settings 📝 (MainMenu.cs): Add MainMenu script to handle starting and quitting the game 📝 (EditorBuildSettings.asset): Include Title Menu scene in build settings 📝 (EditorUserSettings.asset): Update recently used scene to include Title Menu scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string levelToLoad;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
