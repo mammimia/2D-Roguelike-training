be421f815ef83e74986decc55d9611975e660d74
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-10-01 07:46:39 +0300

    ✨ (LevelExit.cs): Add new script for handling level exits in the game 📝 (LevelExit.cs.meta): Add metadata for LevelExit.cs script to define file format version and other necessary information

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    public string levelToLoad;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
