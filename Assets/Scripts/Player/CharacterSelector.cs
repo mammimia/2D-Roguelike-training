81084611a70de3954f8770a8efb66ba561ca5752
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-11-15 10:02:50 +0300

    ✨ (CharacterSelector.cs): Add a new script for character selection functionality in the game. The script allows players to select a character by pressing the 'E' key when near the character.

---
+++ Assets/Scripts/Player/CharacterSelector.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelector : MonoBehaviour
+{
+    private bool canSelect;
+
+    public GameObject message;
+
+    public PlayerController playerToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canSelect)
+        {
+            Vector3 playerPos = PlayerController.instance.transform.position;
+            Destroy(PlayerController.instance.gameObject);
+            PlayerController newPlayer = Instantiate(playerToSpawn, playerPos, playerToSpawn.transform.rotation);
+            PlayerController.instance = newPlayer;
+            gameObject.SetActive(false);
+
+            CameraController.instance.target = newPlayer.transform;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            message.SetActive(true);
+            canSelect = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            message.SetActive(false);
+            canSelect = false;
+        }
+    }
+}
